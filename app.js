5220e610c20b6869f1f1f01e7a32b89e904559e7
Sayed Md Ashabul Yamin <63644065+sayed-md-ashabul-yamin@users.noreply.github.com>
2023-09-03 21:15:18 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,49 @@
+const apiKey = "cdbe74068f635722fc7349d71f4900da";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather?units=metric";
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+// fetch weather info from given apiURL and apiKey
+// await waits until the task is achieved
+// store the response in json-format in data
+async function checkWeather(city) {
+  // use `` in parameter not single or double quotation
+  const response = await fetch(apiURL + `&q=${city}` + `&appid=${apiKey}`);
+
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    var data = await response.json();
+
+    console.log(data);
+
+    // displaying search result
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML =
+      Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + " %";
+    document.querySelector(".wind").innerHTML = data.wind.speed + " km/h";
+
+    if (data.weather[0].main == "Clouds") {
+      weatherIcon.src = "images/clouds.png";
+    } else if (data.weather[0].main == "Clear") {
+      weatherIcon.src = "images/clear.png";
+    } else if (data.weather[0].main == "Drizzle") {
+      weatherIcon.src = "images/drizzle.png";
+    } else if (data.weather[0].main == "Rain") {
+      weatherIcon.src = "images/rain.png";
+    } else if (data.weather[0].main == "Mist") {
+      weatherIcon.src = "images/mist.png";
+    }
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
+}
+
+// on clicking the search button to search for given city
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
